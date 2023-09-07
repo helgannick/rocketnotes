ec3a8d3d6b8bb9448ddd5a3113786b97d0107848
helgannick <helgannick@gmail.com>
2023-09-07 16:45:24 -0300

    finalized details

--- src/pages/Details/Index.jsx
+++ src/pages/Details/Index.jsx
@@ -1,41 +1,53 @@
-import { Container, Links } from "./styles.js";
 import { Button } from "../../components/Button";
 import { ButtonText } from "../../components/ButtonText";
 import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
 import { Tags } from "../../components/Tags";
+import { Container, Content, Links } from "./styles.js";
 
 export function Details() {
-
   return (
     <Container>
+      <Header />
 
-      <Header/>
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
 
-      <ButtonText title='Excluir nota'/>
+          <h1>Introdução ao React</h1>
 
-      <Section title='Links úteis'>
-        <Links>
-          <li> < a href="">https://www.rocketseat.com.br </a></li>
-          <li> < a href="">https://www.rocketseat.com.br </a></li>
-          
-        </Links>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            officia facilis dolor aliquam dolores illo quo maiores eligendi
+            dicta, minus pariatur autem excepturi esse, beatae deleniti
+            perspiciatis vitae sit fugit! Lorem ipsum dolor sit, amet
+            consectetur adipisicing elit. Voluptates ab minima numquam mollitia
+            nostrum quod dignissimos qui maxime quia, soluta necessitatibus
+            illum, accusantium laborum odit itaque rerum exercitationem
+            architecto deserunt. Lorem ipsum dolor sit amet 
+          </p>
 
-      </Section>
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                {" "}
+                <a href="">https://www.rocketseat.com.br </a>
+              </li>
+              <li>
+                {" "}
+                <a href="">https://www.rocketseat.com.br </a>
+              </li>
+            </Links>
+          </Section>
 
-      <Section title='Marcadores'>
-        <Tags title='express'/>
-        <Tags title='nodejs'/>
+          <Section title="Marcadores">
+            <Tags title="express" />
+            <Tags title="nodejs" />
+          </Section>
 
-      </Section>
-
-
-      
-
-     <Button title='Voltar'/>
-
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
-       
-
   );
 }
