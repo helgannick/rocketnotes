88866834a5f700d0177b5d81dd7a558569444ce7
helgannick <helgannick@gmail.com>
2023-09-07 23:27:25 -0300

    create logo

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,32 @@
+import { Container, Brand, Menu, Search, Content, NewNote } from './styles.js'
+
+import { Header } from '../../components/Header'
+
+export function Home(){
+  return (
+    <Container>
+      <Brand>
+        <h1>Rocketnotes</h1>
+      </Brand>
+
+      <Header/>
+
+      <Menu>
+
+      </Menu>
+
+      <Search>
+
+      </Search>
+
+      <Content>
+
+      </Content>
+
+      <NewNote>
+
+      </NewNote>
+
+    </Container>
+  )
+}
