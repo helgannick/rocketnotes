0c945f7ccf3429d3c74b79eca5c5132f29502f65
helgannick <helgannick@gmail.com>
2023-09-09 19:16:45 -0300

    create Note

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,41 +1,56 @@
-import { FiPlus, FiSearch } from 'react-icons/fi';
-import { Container, Brand, Menu, Search, Content, NewNote } from './styles.js'
+import { FiPlus, FiSearch } from "react-icons/fi";
+import { Brand, Container, Content, Menu, NewNote, Search } from "./styles.js";
 
-import { Header } from '../../components/Header'
-import { ButtonText } from '../../components/ButtonText'
-import { Input } from '../../components/Input'
+import { ButtonText } from "../../components/ButtonText";
+import { Header } from "../../components/Header";
+import { Note } from "../../components/Note";
+import { Section } from "../../components/Section";
 
+import { Input } from "../../components/Input";
 
-export function Home(){
+export function Home() {
   return (
     <Container>
       <Brand>
         <h1>Rocketnotes</h1>
       </Brand>
 
-      <Header/>
+      <Header />
 
       <Menu>
-        <li><ButtonText title='Todos'/></li>
-        <li><ButtonText title='React'/></li>
-        <li><ButtonText title='Nodejs'/></li>
-
-
+        <li>
+          <ButtonText title="Todos" />
+        </li>
+        <li>
+          <ButtonText title="React" />
+        </li>
+        <li>
+          <ButtonText title="Nodejs" />
+        </li>
       </Menu>
 
       <Search>
-        <Input placeholder='Pesquisar pelo título ' icon={FiSearch}/>
+        <Input placeholder="Pesquisar pelo título " icon={FiSearch} />
       </Search>
 
       <Content>
-
+        <Section title="Minhas notas">
+          <Note
+            data={{
+              title: "React",
+              tags: [
+                { id: "1", name: "react" },
+                { id: "2", name: "rocketseat" },
+              ],
+            }}
+          />
+        </Section>
       </Content>
 
       <NewNote>
-       <FiPlus/>
+        <FiPlus />
         Criar nota
       </NewNote>
-
     </Container>
-  )
+  );
 }
