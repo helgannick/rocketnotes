7bfbaa014b5310e90befb4b1b6524ebca607b823
helgannick <helgannick@gmail.com>
2023-09-11 04:48:40 -0300

    New ok

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,9 +1,11 @@
 import { Container, Form } from './styles';
+
 import { Header } from '../../components/Header' 
 import { Input } from '../../components/Input';
 import { Textarea } from '../../components/Textarea';
 import { NoteItem } from '../../components/NoteItem';
 import { Section } from '../../components/Section'
+import { Button } from '../../components/Button';
 
 
 export function New(){
@@ -26,6 +28,16 @@ export function New(){
             <NoteItem isNew placeholder='Novo Link'/>
 
           </Section>
+
+          <Section title='Marcadores'>
+            <div className='tags'>
+            <NoteItem value="React" />
+            <NoteItem isNew placeholder='Nova Tag'/>
+            </div>
+
+          </Section>
+
+          <Button title='Salvar'/>
 
         </Form>
       </main>
