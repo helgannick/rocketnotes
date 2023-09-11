c32446e6d2dab78b0144b1abc70d37c91b1d253d
helgannick <helgannick@gmail.com>
2023-09-10 23:08:30 -0300

    create avatar

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,5 +1,5 @@
-import { FiArrowLeft, FiUser, FiMail, FiLock } from "react-icons/fi";
-import { Container, Form } from "./styles";
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
+import { Container, Form, Avatar } from "./styles";
 
 import { Button } from "../../components/Button"
 import { Input } from "../../components/Input";
@@ -14,6 +14,21 @@ export function Profile() {
       </header>
 
       <Form>
+
+        <Avatar>
+          <img 
+          src="https://github.com/helgannick.png" 
+          alt="Foto do usuário"/>
+
+          <label htmlFor="avatar">
+            <FiCamera/>
+            <input
+              id="avatar"
+              type="file"
+            />
+          </label>
+        </Avatar>
+
         <Input 
         placeholder="Nome" 
         type="text" 
