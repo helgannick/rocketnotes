32811356edb35f42d6d2db9069b1b636b40c7074
helgannick <helgannick@gmail.com>
2023-09-11 03:07:09 -0300

    create page New

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,14 @@ import GlobalStyles from './styles/global'
 
 import theme from './styles/theme'
 
-import { Profile } from './pages/Profile/index'
+import { New } from './pages/New/index'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-     <Profile/>
+     <New/>
     </ThemeProvider>
   </React.StrictMode>,
 )
