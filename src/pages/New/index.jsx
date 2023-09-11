d4f859c49450f23bcfde932b14ca136798d31268
helgannick <helgannick@gmail.com>
2023-09-11 03:16:56 -0300

    header-New

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,11 +1,24 @@
-import { Container } from './styles';
+import { Container, Form } from './styles';
 import { Header } from '../../components/Header' 
+import { Input } from '../../components/Input';
 
 
 export function New(){
   return(
     <Container>
       <Header/>
+
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href='/'>voltar</a>
+          </header>
+
+          <Input placeholder='Titulo'/>
+
+        </Form>
+      </main>
     </Container>
   );
 }
