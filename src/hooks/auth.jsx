779f19dcdda69b87266618cec05a89edf2057e10
helgannick <helgannick@gmail.com>
2023-09-26 06:40:05 -0300

    create logout

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 import { api } from "../services/api";
 
@@ -13,6 +13,9 @@ function AuthProvider ({ children }){
     const response = await api.post('/sessions', {email, password});
     const { user, token } = response.data; 
 
+    localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
+    localStorage.setItem("@rocketnotes:token", token);
+
     api.defaults.headers.authorization = `Bearer ${token}`;
     setData({user,token})
 
@@ -25,8 +28,32 @@ function AuthProvider ({ children }){
       }
     }
   }
+
+  function signOut (){
+    localStorage.removeItem("@rocketnotes:token");
+    localStorage.removeItem("@rocketnotes:user");
+
+    setData({});
+
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem("@rocketnotes:token");
+    const user = localStorage.getItem("@rocketnotes:user");
+
+    if(token && user ){
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
+      setData({
+        token,
+        user: JSON.parse(user)
+      })
+    }
+
+  }, []);
+
   return(
-    <AuthContext.Provider value={{ signIn, user: data.user }}>
+    <AuthContext.Provider value={{ signIn,signOut, user: data.user }}>
     {children}
     </AuthContext.Provider>
   )
