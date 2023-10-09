0c5fb88cfec6468d79d50e813a55bcdafae319b7
helgannick <helgannick@gmail.com>
2023-10-09 07:47:00 -0300

    Adding link in note

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,47 +1,67 @@
-import { Container, Form } from './styles';
+import { Container, Form } from "./styles";
+import { useState } from "react";
 
-import { Header } from '../../components/Header' 
-import { Input } from '../../components/Input';
-import { Textarea } from '../../components/Textarea';
-import { NoteItem } from '../../components/NoteItem';
-import { Section } from '../../components/Section'
-import { Button } from '../../components/Button';
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import { Textarea } from "../../components/Textarea";
+import { NoteItem } from "../../components/NoteItem";
+import { Section } from "../../components/Section";
+import { Button } from "../../components/Button";
 
 import { Link } from "react-router-dom";
 
+export function New() {
+  const [links, setLinks] = useState([]);
+  const [newLink, setNewLink] = useState("");
 
+  function handleAddLink() {
+    setLinks((prevState) => [...prevState, newLink]);
+    setNewLink("");
+  }
 
-export function New(){
-  return(
+  return (
     <Container>
-      <Header/>
+      <Header />
 
       <main>
         <Form>
           <header>
             <h1>Criar nota</h1>
-            <Link to='/'>voltar</Link>
+            <Link to="/">voltar</Link>
           </header>
 
-          <Input placeholder='Titulo'/>
-          <Textarea placeholder='Observações'/>
+          <Input placeholder="Titulo" />
+          <Textarea placeholder="Observações" />
 
-          <Section title='Links úteis'>
-            <NoteItem value="test" />
-            <NoteItem isNew placeholder='Novo Link'/>
+          <Section title="Links úteis">
+            {
+            
+            links.map((link, index) => (
+              <NoteItem
+                  key={String(index)}
+                  value={link}
+                  onClick={() => {}}
+                />
+              ))
+            }
 
+            <NoteItem
+              isNew
+              placeholder="Novo Link"
+              value={newLink}
+              onChange={(e) => setNewLink(e.target.value)}
+              onClick={handleAddLink}
+            />
           </Section>
 
-          <Section title='Marcadores'>
-            <div className='tags'>
-            <NoteItem value="React" />
-            <NoteItem isNew placeholder='Nova Tag'/>
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="React" />
+              <NoteItem isNew placeholder="Nova Tag" />
             </div>
-
           </Section>
 
-          <Button title='Salvar'/>
-
+          <Button title="Salvar" />
         </Form>
       </main>
     </Container>
