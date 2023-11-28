7d07ed0c65b623c637a7c45eb1ecd55e3fb94a80
helgannick <helgannick@gmail.com>
2023-11-28 15:21:45 -0300

    Handling Unavailable Routes

--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -1,14 +1,18 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 import { SignUp } from "../pages/SignUp";
 import { SignIn } from "../pages/SignIn";
 
 export function AuthRoutes() {
+  const user = localStorage.getItem("@rocketnotes:user");
+
+
   return (
     <Routes>
       <Route path="/" element={<SignIn />} />
       <Route path="/register" element={<SignUp />} />
       
+      { !user && <Route path="*" element={<Navigate to="/"/>} />}
     </Routes>
   );
 }
